3d4525ac12eb54c6e2642fef6606b0e82429ce8b
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-02-17 07:54:45 +0700

    Update order.route.js

--- back-end/route/order.route.js
+++ back-end/route/order.route.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controller/order.controller');
+
+router.post('/create', orderController.create);
+
+module.exports = router;
