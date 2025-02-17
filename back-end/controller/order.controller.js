ecee396d3e7e607e1d7f0233c875ac57110d5505
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-02-17 07:49:22 +0700

    Update order.controller.js

--- back-end/controller/order.controller.js
+++ back-end/controller/order.controller.js
@@ -1,3 +1,22 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {order, bill, dish} = require('../models');
+
+exports.create = async (req, res) => {
+    const {order_id, total_price, created_by, method, customer_phone_num} = req.body;
+    try {
+        const newBill = new bill({
+        order_id,
+        total_price,
+        created_by,
+        method,
+        customer_phone_num
+        });
+        await newBill.save();
+        res.status(201).json(newBill);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    };
+    
