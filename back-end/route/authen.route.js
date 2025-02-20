af16472c446646983668de6934a078127d116a9a
quanpdhe170415 <125384493+quanpdhe170415@users.noreply.github.com>
2025-02-20 17:11:14 +0700

    Quanpd/create function reset password (#5)
    * create function login
    
    create function login
    
    * create function login
    
    create function login
    
    * create function register account
    
    create function register account
    
    ---------
    
    Co-authored-by: quanpdhe170415 <i-quanpd@sojitz-ti.vn>

--- back-end/route/authen.route.js
+++ back-end/route/authen.route.js
@@ -4,5 +4,7 @@ const authenController = require('../controller/authen.controller');
 
 router.post('/login', authenController.login);
 router.post('/google-login', authenController.googleLogin);
+router.post("/register", authenController.register);
+router.get("/verify-email", authenController.verifyEmail);
 
 module.exports = router;
