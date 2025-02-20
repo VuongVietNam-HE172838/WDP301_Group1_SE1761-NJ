7d2de2d60aa3804614f5e6cf8a48d0d093e3fb9b
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-02-20 07:38:46 +0700

    Update order.controller.js

--- back-end/controller/order.controller.js
+++ back-end/controller/order.controller.js
@@ -64,4 +64,43 @@ exports.delete = async (req, res) => {
         res.status(500).send('Server error');
     }
 }
+
+exports.createOrder = async (req, res) => {
+    const {order_id, dish_id, quantity} = req.body;
+    try {
+        const newOrder = new order({
+        order_id,
+        dish_id,
+        quantity
+        });
+        await newOrder.save();
+        res.status(201).json(newOrder);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    };
+
+exports.sendOrder = async (req, res) => {
+    try {
+        const orders = await order.find();
+        res.status(200).json(orders);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.findOrder = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const oneOrder = await order.findById
+        (id);
+        res.status(200).json(oneOrder);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
+
     
