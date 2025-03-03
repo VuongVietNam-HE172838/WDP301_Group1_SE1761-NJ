d942cc8de3788562c0775bd34299a623a9811c90
TuanAnhh-K17HL <anhtuan25703@gmail.com>
2025-03-03 08:41:40 +0700

    sua loi router

--- back-end/route/index.js
+++ back-end/route/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const authenRoute = require('./authen.route');
-const orderRoute = require('./order.route');
-const changepassRoute = require('./changepass.route');
+// const orderRoute = require('./order.route');
+// const changepassRoute = require('./changepass.route');
 
 router.use('/authen', authenRoute);
-router.use('/order', orderRoute);
-router.use('/changepass', changepassRoute);
+// router.use('/order', orderRoute);
+// router.use('/changepass', changepassRoute);
 
 module.exports = router;
