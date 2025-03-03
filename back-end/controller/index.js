7c9432fc43eab21ad62fa092ef7455f3cd6e162c
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-03-03 07:53:17 +0700

    Update index.js

--- back-end/controller/index.js
+++ back-end/controller/index.js
@@ -1,5 +1,7 @@
 const authenController = require('./authen.controller');
+const orderController = require('./order.controller');
+const changepassController = require('./changepass.controller');
 
 module.exports = {
-  authenController,
+  authenController, orderController, changepassController
 };
