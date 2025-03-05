c6cb52844262a0a7d3f895f8c749f15e3e5210c2
VietHE161146fpt <viettqhe161146@fpt.edu.vn>
2025-03-05 22:32:23 +0700

    fix menu, database, blog

--- back-end/route/index.js
+++ back-end/route/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const authenRoute = require('./authen.route');
 const orderRoute = require('./order.route');
 const changepassRoute = require('./changepass.route');
+const menuRoute = require('./menu.routes');
+const blogRoutes = require('./blog.route');
+
 
 router.use('/authen', authenRoute);
 router.use('/order', orderRoute);
 router.use('/changepass', changepassRoute);
-
+router.use('/menu', menuRoute);
+router.use('/blog', blogRoutes);
 module.exports = router;
